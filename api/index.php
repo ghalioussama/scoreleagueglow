27a24b785f45c885c81e520e95a0f3ae68d0d8ed
ghalioussama <110343529+ghalioussama@users.noreply.github.com>
2023-09-15 12:55:26 +0100

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,27 @@
+
+
+<?php
+
+if(array_key_exists("submit",$_GET)){
+  
+//add points to country
+$jsonString = file_get_contents('leaderboard.json');
+$data = json_decode($jsonString, true);
+
+foreach ($data as $key => $entry) {
+    if ($entry['code'] == "MA") {
+      if($_GET["player"]>$_GET["computer"]){
+        $data[$key]['points'] +=3;
+      }
+      else if($_GET["player"]==$_GET["computer"]){
+        $data[$key]['points'] +=1;
+      }
+    }
+    
+}
+
+$newJsonString = json_encode($data);
+file_put_contents('leaderboard.json', $newJsonString);
+}
+
+?>
